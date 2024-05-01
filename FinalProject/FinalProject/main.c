63fce2993b6da9e847292f1bab6b29e32fdefe5c
Marsh <jdmarsh@kite.upenn.edu>
2024-04-30 21:01:14 -0400

    from new computer

---
+++ FinalProject/FinalProject/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 4/30/2024 8:07:39 PM
+ *  Author: jdmarsh
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
